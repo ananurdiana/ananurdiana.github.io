273e124415d6328d8191627f7fe875d59b660573
Ana Nurdiana <ananurdiana@gmail.com>
2019-11-25 13:02:33 +0700

    Update main.blog.js

--- js/main.blog.js
+++ js/main.blog.js
@@ -1,7 +1,7 @@
 (function(){
   // side menu
   for(var i = 0; i< $('h5').length; i++){
-    a = $('h5:eq('+i+')').html();
+    a = $('h5:eq('+i+')').text();
     b = a.toLowerCase();
     id = b.replace(/ /g, "-");
     $(".navbar-nav").append('<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#'+id+'">'+a+'</a></li>');
